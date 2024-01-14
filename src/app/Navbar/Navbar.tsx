71f6611e930dea4e6b86c47ca8a01260a237b7e9
Muneeb ur rehman <bhatmuneeb0711@gmail.com>
2024-01-14 18:50:50 +0530

    added add to cart button

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <div className="bg-base-100"> \
+         <div className="navbar max-w-7xl m-auto flex-col sm:flex-row- gap-2">
+
+         </div>
+        </div>
+    )
+
+}
---
+++ src/app/products/[id]/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { prisma } from '@/lib/db/prisma';
+import { createCart, getCart } from "@/lib/db/cart";
+import { revalidatePath } from 'next/cache';
+
+
+export async function incrementProductQuantity(productId: string) {
+    const cart = (await getCart()) ?? (await createCart());
+    
+    const articleInCart = cart.items.find((item) => item.productId === productId);
+    if (articleInCart) {
+        await prisma.cartItem.update({
+            where: { id: articleInCart.id },
+            data: { quantity: { increment: 1 } }
+
+        });
+    }
+    else {
+        await prisma.cartItem.create({
+            data: {
+                cartId: cart.id,
+                productId,
+                quantity: 1,
+            },
+        });
+    }
+
+    revalidatePath("/Products/[id]");
+
+}
+
